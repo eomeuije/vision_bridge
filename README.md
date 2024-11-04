3dfc2ca9c6b720a8c5d3d9e93b47008c9655ae4d
eomeuije <euije6795@gmail.com>
2024-11-04 19:22:27 +0900

    프로젝트 init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vision_bridge
 ## 창원대학교 캡스톤디자인 점자번역 서비스
+### 실행방법
+```txt
+build.gradle의 dependencies 다운로드 후 src/main 아래의 VisionBridgeApplication.java(class) main함수 실행
+```
---
+++ src/main/java/org/vision/bridge/VisionBridgeApplication.java
@@ -0,0 +1,13 @@
+package org.vision.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionBridgeApplication.class, args);
+    }
+
+}
